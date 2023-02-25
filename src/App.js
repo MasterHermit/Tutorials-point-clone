98c7ece3c41bb5e472cd099d0a8d833ed29f2dcc
MasterHermit <rajpruthwi5534.ps@gmail.com>
2023-02-26 00:46:05 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './components/navbar/Navbar'
-import Navbar from './components/navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Jobs from "./pages/Jobs/Jobs";
+import Quiz from "./pages/Quiz/Quiz";
+import Category from "./pages/Category/Category";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,46 +1,67 @@
-import { FaCode, FaHome, FaBriefcase, FaFacebook, FaInstagram, FaTwitter, FaYoutube, FaLinkedinIn } from "react-icons/fa"
-import { SiGoogleclassroom } from "react-icons/si"
-import { BiCategory } from 'react-icons/bi'
-import ToggleButton from 'react-toggle-button'
-import { useState } from "react"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  FaCode,
+  FaHome,
+  FaBriefcase,
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaYoutube,
+  FaLinkedinIn,
+} from "react-icons/fa";
+import { SiGoogleclassroom } from "react-icons/si";
+import { BiCategory } from "react-icons/bi";
+import ToggleButton from "react-toggle-button";
 
 const Navbar1 = () => {
-
-  const [ toggle, setToggle ] = useState( false )
+  const [toggle, setToggle] = useState(false);
 
   return (
     <div className="flex bg-zinc-300 space-x-80 ">
-      {/* left container */ }
+      {/* left container */}
       <div className="flex">
-        <img src="https://images.ctfassets.net/hrltx12pl8hq/3MiLYuOESJbXrwoeXUI012/ccba97119444af2f25aa9cfddaf844bf/05-logos_548082964.jpg?fit=fill&w=480&h=270&fm=webp" alt="logo"
+        <img
+          src="https://images.ctfassets.net/hrltx12pl8hq/3MiLYuOESJbXrwoeXUI012/ccba97119444af2f25aa9cfddaf844bf/05-logos_548082964.jpg?fit=fill&w=480&h=270&fm=webp"
+          alt="logo"
           className="object-cover h-10"
         />
-        <div className="flex flex-initial items-center pl-10 pr-5 ">
-          <FaHome className="pr-2 h-8 w-8" />
-          <p>Home</p>
-        </div>
-        <div className="flex flex-initial items-center pr-10 ">
-          <FaCode className="pr-2 h-8 w-8" />
-          <p>Quizes</p>
-        </div>
-        <div className="flex flex-initial items-center pr-10 ">
-          <FaBriefcase className="pr-2 h-8 w-8" />
-          <p>Jobs</p>
-        </div>
-        <div className="flex flex-initial items-center pr-10 ">
-          <BiCategory className="pr-2 h-8 w-8" />
-          <p>Category</p>
-        </div>
+
+        <Link to="/">
+          <div className="flex flex-initial items-center pl-10 pr-5 ">
+            <FaHome className="pr-2 h-8 w-8" />
+            <p>Home</p>
+          </div>
+        </Link>
+        <Link to="/quiz">
+          <div className="flex flex-initial items-center pr-10 ">
+            <FaCode className="pr-2 h-8 w-8" />
+            <p>Quizes</p>
+          </div>
+        </Link>
+        <Link to="/jobs">
+          <div className="flex flex-initial items-center pr-10 ">
+            <FaBriefcase className="pr-2 h-8 w-8" />
+            <p>Jobs</p>
+          </div>
+        </Link>
+        <Link to="category">
+          <div className="flex flex-initial items-center pr-10 ">
+            <BiCategory className="pr-2 h-8 w-8" />
+            <p>Category</p>
+          </div>
+        </Link>
       </div>
-      {/* left container ends */ }
-      {/* right container */ }
+      {/* left container ends */}
+      {/* right container */}
       <div className="flex pl-36">
         <div className="flex flex-initial items-center pr-10">
           <ToggleButton
-            value={ toggle }
-            onToggle={ () => {
-              setToggle( value => !value );
-            } } />
+            value={toggle}
+            onToggle={() => {
+              setToggle((value) => !value);
+            }}
+          />
         </div>
         <div className="flex flex-initial items-center pr-10 ">
           <SiGoogleclassroom className="pr-2 h-8 w-8" />
@@ -54,12 +75,17 @@ const Navbar1 = () => {
           <FaLinkedinIn />
         </div>
         <div className="flex items-center">
-          <button type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center mr-2  dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">Login</button>
+          <button
+            type="button"
+            className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center mr-2  dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
+          >
+            Login
+          </button>
         </div>
       </div>
-      {/* right container ends */ }
+      {/* right container ends */}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar1
+export default Navbar1;
