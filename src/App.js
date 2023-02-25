6a1704d30aa3bdc42970f57af70a79919c18f496
MasterHermit <rajpruthwi5534.ps@gmail.com>
2023-02-26 00:51:06 +0530

    app conflict resolved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
-import './components/navbar/Navbar'
-import Navbar from './components/navbar/Navbar';
-import StatsCard from './components/web_stats/StatsCard';
-=======
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Jobs from "./pages/Jobs/Jobs";
 import Quiz from "./pages/Quiz/Quiz";
 import Category from "./pages/Category/Category";
->>>>>>> 98c7ece3c41bb5e472cd099d0a8d833ed29f2dcc
+
 
 function App () {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Navbar />
-      <StatsCard />
-=======
+
       <Router>
         <Navbar />
         <Routes>
@@ -27,7 +20,7 @@ function App () {
           <Route path="/quiz" element={<Quiz />} />
         </Routes>
       </Router>
->>>>>>> 98c7ece3c41bb5e472cd099d0a8d833ed29f2dcc
+
     </div>
   );
 }
