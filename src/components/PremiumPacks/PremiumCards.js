2f4d9ec68f48731455c3395b190316cabab49c04
MasterHermit <rajpruthwi5534.ps@gmail.com>
2023-02-26 11:50:14 +0530

    data fetch to premiumcards

--- src/components/PremiumPacks/PremiumCards.js
+++ src/components/PremiumPacks/PremiumCards.js
@@ -1,11 +1,19 @@
-var data = require( '../../../db.json' )
+import { useFetchCoursesQuery } from "../../store";
+
 
 const PremiumCards = () => {
-    return (
-        <div>
-            { data.courses.aiml.c_name }
-        </div>
-    )
-}
+  const { data, error, isLoading } = useFetchCoursesQuery();
+  let content;
+  if (isLoading) {
+    content = <div>Loading..</div>;
+  } else if (error) {
+    content = <div>{error}</div>;
+  } else {
+    content = data;
+  }
+  console.log(content);
 
-export default PremiumCards
+  return <div>PremiumCards</div>;
+};
+
+export default PremiumCards;
