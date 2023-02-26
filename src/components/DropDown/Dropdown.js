d9a6c72053df385665dcc8f95f143b2a1e33f13c
ankush9054 <dasankush1511@gmail.com>
2023-02-26 17:26:58 +0530

    dropdown added and courseCard optimised

--- src/components/DropDown/Dropdown.js
+++ src/components/DropDown/Dropdown.js
@@ -1,5 +1,61 @@
-function DropDown (){
-    return <div>
-        DropDown
-    </div>
-}
+import { React, useState } from 'react'
+import './DropDown.css'
+
+const Dropdown = () => {
+    const [ open, setOpen ] = useState( false );
+
+    const handleOpen = () => {
+        setOpen( !open );
+    };
+
+    const handleMenuOne = () => {
+        // do something
+        console.log( 'clicked one' );
+    };
+
+    const handleMenuTwo = () => {
+        // do something
+        console.log( 'clicked two' );
+    };
+
+    return (
+        <Drop
+            trigger={ <button>Dropdown</button> }
+            menu={ [
+                <button onClick={ handleMenuOne }>Menu 1</button>,
+                <button onClick={ handleMenuTwo }>Menu 2</button>,
+            ] }
+        />
+    );
+};
+
+const Drop = ( { trigger, menu } ) => {
+    const [ open, setOpen ] = React.useState( false );
+
+    const handleOpen = () => {
+        setOpen( !open );
+    };
+
+    return (
+        <div className="dropdown">
+            { React.cloneElement( trigger, {
+                onClick: handleOpen,
+            } ) }
+            { open ? (
+                <ul className="menu">
+                    { menu.map( ( menuItem, index ) => (
+                        <li key={ index } className="menu-item">
+                            { React.cloneElement( menuItem, {
+                                onClick: () => {
+                                    menuItem.props.onClick();
+                                    setOpen( false );
+                                },
+                            } ) }
+                        </li>
+                    ) ) }
+                </ul>
+            ) : null }
+        </div>
+    );
+};
+export default Dropdown
--- src/components/PremiumPacks/CourseCards.js
+++ src/components/PremiumPacks/CourseCards.js
@@ -1,29 +1,32 @@
-import { FaUser, FaExternalLinkAlt } from 'react-icons/fa'
+import { FaUser, FaExternalLinkAlt, FaPlayCircle } from 'react-icons/fa'
 
 import './CourseCard.css'
 
 
 const CourseCards = ( { category } ) => {
     return (
-        <div className='main_card ml-20'>
+        <div className='main_card ml-20 rounded-md '>
             <div className='thumb'>
                 <img src={ category.thumb } alt='coursePic' />
             </div>
             <div className='pl-3'>
-                <h2 className='font-medium mb-8'>{ category.c_name }</h2>
+                <div className='flex flex-initial items-center justify-between'>
+                    <h2 className='font-medium mb-8 mt-2 text-xl'>{ category.c_name }</h2>
+                    <h2 className='font-medium mb-8 mt-2 text-xl'>{ category.value }</h2>
+                </div>
                 <div className='flex flex-initial items-center'>
                     <FaUser />
-                    <span className='mx-2 font-normal'>Ankush Das</span>
+                    <span className='mx-2 font-semibold '>{ category.author }</span>
                     <FaExternalLinkAlt />
                 </div>
             </div>
-            <hr />
-            <div className='flex flex-row justify-between'>
-                <p className='ml-3'>6 Lecture</p>
-                <p className='mr-3'>rating</p>
+            <div className='my-3'>
+                <hr />
             </div>
-            <div className='pb-3'>
-                <p className='font-bold'>Paid</p>
+            <div className='flex flex-row items-center pl-3 pb-3'>
+                <FaPlayCircle />
+                <p className='mx-2'>{ category.no_of_lec } Lecture</p>
+                <p className='ml-20'>rating</p>
             </div>
         </div>
     )
