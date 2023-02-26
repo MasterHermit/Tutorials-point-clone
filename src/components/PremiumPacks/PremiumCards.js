57c59fa8317f78dd1d559c48d97157946faa6c53
MasterHermit <rajpruthwi5534.ps@gmail.com>
2023-02-26 12:44:16 +0530

    db.json updated

--- src/components/PremiumPacks/PremiumCards.js
+++ src/components/PremiumPacks/PremiumCards.js
@@ -1,22 +1,18 @@
 import { useFetchCoursesQuery } from "../../store";
 
-
 const PremiumCards = () => {
   const { data, error, isLoading } = useFetchCoursesQuery();
   let content;
-  if ( isLoading ) {
+  if (isLoading) {
     content = <div>Loading..</div>;
-  } else if ( error ) {
-    content = <div>{ error }</div>;
+  } else if (error) {
+    content = <div>{error}</div>;
   } else {
-    content = data.map( ( category ) => {
-      console.log( category );
-    } )
+    content = data.map((category) => {
+      console.log(category);
+    });
   }
-  return (
-    <div>
-    </div>
-  )
+  return <div></div>;
 };
 
 export default PremiumCards;
