edf62bd6dd5c0dc35a78ba857f1d96d6aa3eced8
ankush9054 <dasankush1511@gmail.com>
2023-02-26 23:07:26 +0530

    Done with jobs card and configured jit mode

--- src/pages/Jobs/Jobs.js
+++ src/pages/Jobs/Jobs.js
@@ -1,18 +1,29 @@
 import React from "react";
+import JobsCard from "../../components/Jobs_components/JobsCard";
+import JobsSideBar from "../../components/Jobs_components/JobsSideBar";
 import { useFetchJobsQuery } from "../../store";
 
 const Jobs = () => {
   const { data, error, isLoading } = useFetchJobsQuery();
   let jobs;
-  if (isLoading) {
+  if ( isLoading ) {
     jobs = <div>Loading..</div>;
-  } else if (error) {
-    jobs = <div>{error}</div>;
+  } else if ( error ) {
+    jobs = <div>{ error }</div>;
   } else {
-    // jobs = data;
-    console.log(data);
+    jobs = data.map( ( job ) => {
+      return <JobsCard key={ job.id } job={ job } />
+    } )
+
   }
-  return <div>Jobs</div>;
+  return (
+    <div className="flex flex-row">
+      <JobsSideBar />
+      <div className="flex flex-col">
+        { jobs }
+      </div>
+    </div>
+  )
 };
 
 export default Jobs;
