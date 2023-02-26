d6188158617e7051bfd9f5ba4137a3f0bf52f704
ankush9054 <dasankush1511@gmail.com>
2023-02-26 12:26:01 +0530

    premium

--- src/components/PremiumPacks/PremiumCards.js
+++ src/components/PremiumPacks/PremiumCards.js
@@ -3,22 +3,18 @@ import { useFetchCoursesQuery } from "../../store";
 
 const PremiumCards = () => {
   const { data, error, isLoading } = useFetchCoursesQuery();
-  let apiData
   let content;
   if ( isLoading ) {
     content = <div>Loading..</div>;
   } else if ( error ) {
     content = <div>{ error }</div>;
   } else {
-    content = data
-    apiData = Object.keys( content )
-    apiData.map( ( category ) => {
-      return category
+    content = data.map( ( category ) => {
+      console.log( category );
     } )
   }
   return (
     <div>
-      { apiData }
     </div>
   )
 };
